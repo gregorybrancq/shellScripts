55cb34de87e863f4d00f29d4409a41ff5bd55041
Gregory Brancq <gregory.brancq@free.fr>
2014-09-05 09:35:56 +0200

    Added script to backup KMyMoney files

---
+++ scripts/backupKMyMoney.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+# -*-coding:Latin-1 -*
+
+'''
+Backup KMyMoney files
+'''
+
+
+
+## Import
+import sys
+import os, os.path
+import re
+import time, datetime
+import filecmp
+import shutil
+from optparse import OptionParser
+
+## common
+from python_common import *
+HEADER = "backupKMyMoney"
+
+## directory
+homeDir = getHomeDir()
+logDir  = getLogDir()
+
+###############################################
+
+
+
+###############################################
+###############################################
+##              Line Parsing                 ##
+###############################################
+###############################################
+
+parsedArgs = {}
+parser = OptionParser()
+
+
+parser.add_option(
+    "--debug",
+    action  = "store_true",
+    dest    = "debug",
+    default = False,
+    help    = "Display all debug information"
+    )
+
+parser.add_option(
+    "--nogui",
+    action  = "store_false",
+    dest    = "gui",
+    default = True,
+    help    = "Print information in a shell"
+    )
+
+(parsedArgs , args) = parser.parse_args()
+
+###############################################
+
+
+
+
+###############################################
+## Global variables
+###############################################
+
+t = str(datetime.datetime.today().isoformat("_"))
+logFile = os.path.join(logDir, HEADER + "_" + t + ".log")
+lockFile = os.path.join(logDir, HEADER + ".lock")
+warnC = 0
+
+fileBackupList = []
+fileBackupName = ""
+
+fileName = "comptes.kmy"
+if isDell :
+    fileOriginal = os.path.join(homeDir, "Perso/Money/KMyMoney", fileName)
+    fileBackupDir = os.path.join(homeDir, "Perso/Money/KMyMoney/Backup")
+else :
+    fileOriginal = os.path.join(homeDir, "Greg/Money/KMyMoney", fileName)
+    fileBackupDir = os.path.join(homeDir, "Greg/Money/KMyMoney/Backup")
+
+###############################################
+
+
+
+
+###############################################
+###############################################
+##                FUNCTIONS                  ##
+###############################################
+###############################################
+
+def findBackupFiles() :
+    global dbg
+    global fileBackupList
+    dbg.info(HEADER, "In  findBackupFiles")
+    fileBackupList = [ f for f in os.listdir(fileBackupDir) if (os.path.isfile(os.path.join(fileBackupDir,f))) ]
+    dbg.info(HEADER, "Out findBackupFiles fileList=" + str(fileBackupList))
+
+
+def backupToDo() :
+    global dbg
+    global fileBackupName
+    dbg.info(HEADER, "In  backupToDo")
+
+    # find the last backup file name
+    findBackupFiles()
+
+    ## Look if it's necessary to backup
+    doBackup = False
+    for f in fileBackupList :
+        dbg.info(HEADER, "In  backupToDo fileBackup=" + str(f))
+        comp = filecmp.cmp(fileOriginal, os.path.join(fileBackupDir, f))
+        if comp :
+            dbg.info(HEADER, "In  backupToDo fileBackup find")
+            fileBackupName = f
+            doBackup = True
+            break
+
+    if not doBackup :
+        now = datetime.datetime.now()
+        (fileN, extN) = os.path.splitext(fileName)
+        newName = fileN + "_" + str(now.strftime("%Y-%m-%d") + extN)
+        dbg.info(HEADER, "In  backupToDo copy newName=" + str(newName))
+        shutil.copy2(fileOriginal, os.path.join(fileBackupDir, newName))
+
+    dbg.info(HEADER, "Out backupToDo")
+
+###############################################
+
+
+
+
+
+
+###############################################
+###############################################
+###############################################
+##                 MAIN                      ##
+###############################################
+###############################################
+###############################################
+
+
+def main() :
+    global dbg
+    global warnC
+    movieList = list()
+    dbg.info(HEADER, "In  main")
+
+    ## Backup file
+    backupToDo()
+    
+    dbg.info(HEADER, "Out main")
+
+
+
+if __name__ == '__main__':
+ 
+    ## Create log class
+    dbg = LOGC(logFile, HEADER, parsedArgs.debug, parsedArgs.gui)
+
+    main()
+
+###############################################
+
