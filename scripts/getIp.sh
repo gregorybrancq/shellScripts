db9176bf53922163552610e674905f64669042e4
Gregory Brancq <gregory.brancq@free.fr>
2015-12-11 23:24:53 +0100

    Renamed

--- scripts/getIp.sh
+++ scripts/getIp.sh
@@ -1,4 +1,4 @@
-#!/bin/csh -f
+#!/bin/sh -f
 
 echo ; date | cut -f 2 ; echo '##### @IP Publique #####' ; wget http://checkip.dyndns.org/ -O - -o /dev/null | cut -d: -f 2 | cut -d\< -f 1 ; echo '##### @IP Privée eth #####' ; ifconfig | grep Masque | cut -d i -f 2
 
