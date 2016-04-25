4544321f30a4a8d1657f38a46f694c96018139f5
Gregory Brancq <gregory.brancq@free.fr>
2016-04-25 16:16:09 +0200

    Added command to launch as a reminder

--- scripts/sequans.sh
+++ scripts/sequans.sh
@@ -1,8 +1,14 @@
 #!/bin/sh -f
 
+# command to launch to open terminal with good configuration
+#env SEQUANS_VPN=1 roxterm --hide-menubar --geometry=100x30+350+280 --title='OPEN VPN Sequans' -e sequans
+
+
 cd ~/Greg/work/config/vpn/sequans
 more password
 sudo ./connect-France.sh
+
+
 
 #set procPid=`ps -C openvpn -o pid=`
 #set status=$?
