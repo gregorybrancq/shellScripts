ca7c66773070e26bf7ddc2b96b5037fd2fc11e72
Gregory Brancq <gregory.brancq@free.fr>
2016-04-26 13:43:34 +0200

    Renamed file

--- scripts/OpenCellar.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -f
-
-cd /home/greg/Greg/work/config/opencellar
-java -jar "/opt/OpenCellar/OCCP2010Beta2/opencellar.jar"
-#java -jar "/opt/OpenCellar/CrossPlatform/opencellar.jar"
---
+++ scripts/opencellar.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -f
+
+cd /home/greg/Greg/work/config/opencellar
+java -jar "/opt/opencellar/opencellar.jar"
+
