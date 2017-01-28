2db9852f90de9524cbf33a97a80166d36d91e845
Gregory Brancq <gregory.brancq@free.fr>
2017-01-28 15:06:42 +0100

    Restart also the cairo-dock launcher API dameon

--- scripts/cairo-dock-restart.sh
+++ scripts/cairo-dock-restart.sh
@@ -1,9 +1,15 @@
 #!/bin/sh -f
 
-proc=`ps -C cairo-dock -o pid=`
+# process cairo-dock
+proc_cd=`ps -C cairo-dock -o pid=`
 
-echo "Kill cairo-dock (process $proc)"
-kill -9 $proc
+# process cairo-dock-launcher-API-daemon
+proc_cdlad=`ps -C "python /usr/lib/x86_64-linux-gnu/cairo-dock/cairo-dock-launcher-API-daemon" -o pid=`
+
+echo "Kill cairo-dock (process $proc_cd)"
+kill -9 $proc_cd
+echo "Kill cairo-dock-launcher-API-daemon (process $proc_cdlad)"
+kill -9 $proc_cdlad
 
 echo "Launch new cairo-dock"
 cairo-dock &
