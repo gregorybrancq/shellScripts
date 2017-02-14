7af7327e03af08262cd45153460607c3637d82f7
Gregory Brancq <gregory.brancq@free.fr>
2017-02-14 10:06:34 +0100

    Added log file. Added args.

--- scripts/dodoJuju.sh
+++ scripts/dodoJuju.sh
@@ -9,23 +9,57 @@
 #   - Blocks keyboard and mouse
 # 
 
+# Log file
+logF="/home/greg/Greg/work/env/log/dodoJuju_`date +%Y-%m-%d:%H:%m:%S.%N`.log"
 
 # Enable
-enable="1"
+enable=1
+block=0
+unblock=0
 
 # To have these informations :
 #   > xinput -list
-keyboardId="10"
-mouseId="9"
+keyboardId=10
+mouseId=9
 
 # Programs list
 prgs="totem vlc mplayer"
 
 # Day & Hour
-beginDay="1"
-endDay="5"
-beginHour="1"
-endHour="6"
+beginDay=1
+endDay=5
+beginHour=1
+endHour=6
+
+
+
+# 
+# Args
+#
+
+for i in "$@"
+do
+case $i in
+    -b|--block)
+    block=1
+    shift # past argument=value
+    ;;
+    -u|--unblock)
+    unblock=1
+    shift # past argument=value
+    ;;
+    -h|--help)
+    echo "**********\n** HELP **\n**********"
+    echo " -b|--block     block the keyboard and mouse"
+    echo " -u|--unblock   unblock the keyboard and mouse"
+    exit 0
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
+
 
 
 # 
@@ -39,12 +73,12 @@ checkDateHour() {
     day=`date +%u`
     hour=`date +%H`
     min=`date +%M`
-    echo "Day=$day Hour=$hour Min=$min"
+    echo "Day=$day Hour=$hour Min=$min" >> $logF
 
     beginDayTmp=`expr $beginDay - 1`
     endDayTmp=`expr $endDay + 1`
     beginHourTmp=`expr $beginHour - 1`
-    echo "beginDay=$beginDayTmp endDay=$endDayTmp beginHour=$beginHourTmp endHour=$endHour"
+    echo "beginDay=$beginDayTmp endDay=$endDayTmp beginHour=$beginHourTmp endHour=$endHour" >> $logF
 
     if [ $day -gt $beginDayTmp ] && [ $day -lt $endDayTmp ]; then
         if [ $hour -gt $beginHourTmp ] && [ $hour -lt $endHour ]; then
@@ -60,7 +94,7 @@ checkDateHour() {
 }
 
 killPrgs() {
-    echo "Kill progs"
+    echo "Kill progs" >> $logF
     for prog in $prgs
     do
         # Get id
@@ -68,7 +102,7 @@ killPrgs() {
         
         # Kill it
         if [ "$proc" != "" ]; then
-            echo "Prog $prog with pid $proc has been killed."
+            echo "Prog $prog with pid $proc has been killed." >> $logF
             kill -9 $proc        
         fi
     done
@@ -76,14 +110,14 @@ killPrgs() {
 
 
 blockKbMouse() {
-    echo "Block keyboard & mouse"
+    echo "Block keyboard & mouse" >> $logF
     xinput disable $keyboardId
     xinput disable $mouseId
 }
 
 
 unblockKbMouse() {
-    echo "Unblock keyboard & mouse"
+    echo "Unblock keyboard & mouse" >> $logF
     xinput enable $keyboardId
     xinput enable $mouseId
 }
@@ -95,28 +129,31 @@ unblockKbMouse() {
 #
 
 if [ $enable -eq 1 ]; then
+    echo " Main script $0 " >> $logF
 
     # Check if the date/hour allows to execute
     checkDateHour
     varDH=$?
-    echo "varDH = $varDH"
-    if [ $varDH -eq 1 ]; then
-        echo "Kill and block"
+    echo "varDH = $varDH" >> $logF
+    if [ $varDH -eq 1 ] || [ $block -eq 1 ]; then
+        echo "Kill and block" >> $logF
 
-        # Kill the different programs
-        killPrgs
+        if [ $varDH -eq 1 ]; then
+            # Kill the different programs
+            killPrgs
+        fi
 
         # Block keyboard and mouse
         blockKbMouse
 
-    elif [ $varDH -eq 2 ]; then
-        echo "Unblock"
+    elif [ $varDH -eq 2 ] || [ $unblock -eq 1 ]; then
+        echo "Unblock" >> $logF
 
         # Unblock keyboard and mouse
         unblockKbMouse
 
     else
-        echo "Do nothing"
+        echo "Do nothing" >> $logF
 
     fi
 fi
