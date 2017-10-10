83001698d71043ee4e9c32ee792f8b7176c14369
Gregory Brancq <gregory.brancq@free.fr>
2017-10-10 16:52:57 +0200

    Switch all opened sinks

--- scripts/switchAudio.sh
+++ scripts/switchAudio.sh
@@ -3,27 +3,27 @@
 # Determine which interface is playing
 defSink=`pacmd list-sinks | grep -e 'name:' -e 'index' | grep "*" | awk -F "index: " '{print $NF}'`
 echo "defSink = $defSink"
-defIndex0=`pacmd list-sink-inputs | grep -m1 'index' | awk -F 'index: ' '{print $NF}'`
-echo "defIndex0 = $defIndex0"
-defIndex1=`pacmd list-sink-inputs | grep -m2 'index' | grep -v "$defIndex0" | awk -F 'index: ' '{print $NF}'`
-echo "defIndex1 = $defIndex1"
+# Get back open sinks
+defIndexS=`pacmd list-sink-inputs | grep 'index' | awk -F 'index: ' '{print $NF}'`
+echo "defIndexS = $defIndexS"
+
 
 # Switch audio interface
 if [ $defSink -eq 0 ] ; then
     echo "Switch to Focal"
     # Focal XS
-    pacmd set-default-sink 1 
-    # For video
-    pacmd move-sink-input $defIndex1 1 
-    # For music
-    pacmd move-sink-input $defIndex0 1 
-else 
+    pacmd set-default-sink 1
+    # For all open sink
+    for defIndex in $defIndexS; do
+        pacmd move-sink-input $defIndex 1
+    done
+else
     echo "Switch to Headphone"
     # Headphones
-    pacmd set-default-sink 0 
-    # For video
-    pacmd move-sink-input $defIndex1 0 
-    # For music
-    pacmd move-sink-input $defIndex0 0 
+    pacmd set-default-sink 0
+    # For all open sink
+    for defIndex in $defIndexS; do
+        pacmd move-sink-input $defIndex 0
+    done
 fi
 
