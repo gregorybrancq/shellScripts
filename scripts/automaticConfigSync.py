d3d24fdef20342c27d3a331f30122b7e46eaf575
Gregory Brancq <gregory.brancq@free.fr>
2018-06-26 11:14:18 +0200

    Better way to find unison configuration

--- scripts/automaticConfigSync.py
+++ scripts/automaticConfigSync.py
@@ -4,21 +4,40 @@
 # June 2018
 # Public domain software
 
-import os
-import subprocess
-
-
 """
 Program to check which unison configuration using and launch the synchronisation
 """
     
+
+import os
+import subprocess
+import socket
+
+addressCorres = dict()
+addressCorres["192.168.0.1"] = "portable_room"
+addressCorres["10.42.0.1"]   = "portable"
+addressCorres["192.168.0.2"] = "home"
+addressCorres["10.42.0.146"] = "home_desk"
+
 addressConfig = dict()
 addressConfig["home"] = "room_server_sata"
 addressConfig["home_desk"] = "dockstation_server_sata"
-addressConfig["home_vpn"] = "office_server_sata"
 addressConfig["portable"] = "server_dockstation_sata"
 addressConfig["portable_room"] = "server_room_sata"
 
+
+def getIp():
+    #print([(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1])
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
 
 def checkAddress(ad):
@@ -46,11 +65,14 @@ def runSync(cfg):
 
 
 def main():
-    adCfgKeys = addressConfig.keys()
-    for adCfgKey in ["home", "home_desk", "portable_room", "portable", "home_vpn"] :
-        if checkAddress(adCfgKey) :
-            runSync(addressConfig[adCfgKey])
-            break
+    myIp=getIp()
+    print "My IP="+str(myIp)
+    remoteIp=addressCorres[myIp]
+    print "My remote ip="+str(remoteIp)
+    if checkAddress(remoteIp) :
+        cfg=addressConfig[remoteIp]
+        print "My config="+str(cfg)
+        runSync(cfg)
 
 
 
