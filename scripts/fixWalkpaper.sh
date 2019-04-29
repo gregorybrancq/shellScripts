8da45587d5fa3fe14b840c542645c30c0a48064c
Gregory Brancq <gregory.brancq@free.fr>
2019-04-29 12:02:57 +0200

    No need to have a title for the window

--- scripts/fixWalkpaper.sh
+++ scripts/fixWalkpaper.sh
@@ -6,6 +6,6 @@ sleep 5s
 curDesktop=`xdotool get_desktop`
 xdotool set_desktop 1
 #dconf read /org/gnome/shell/extensions/walkpaper/workspace-wallpapers
-roxterm --hide-menubar --geometry=100x30+350+280 --title='OPEN VPN Sequans' -e "dconf write /org/gnome/shell/extensions/walkpaper/workspace-wallpapers \"['file:///home/greg/Wallpaper/Home/desk_1.jpg', 'file:///home/greg/Wallpaper/Home/desk_2.png', 'file:///home/greg/Wallpaper/Home/desk_3.jpg', 'file:///home/greg/Wallpaper/Home/desk_4.jpg', 'file:///home/greg/Wallpaper/Home/desk_5.jpg', 'file:///home/greg/Wallpaper/Home/desk_6.jpg']\""
+roxterm --hide-menubar --geometry=100x30+350+280 -e "dconf write /org/gnome/shell/extensions/walkpaper/workspace-wallpapers \"['file:///home/greg/Wallpaper/Home/desk_1.jpg', 'file:///home/greg/Wallpaper/Home/desk_2.jpg', 'file:///home/greg/Wallpaper/Home/desk_3.png', 'file:///home/greg/Wallpaper/Home/desk_4.jpg', 'file:///home/greg/Wallpaper/Home/desk_5.jpg']\""
 sleep 1s
 xdotool set_desktop $curDesktop
