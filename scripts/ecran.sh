fa122f91d5d16ae536f461f1d80c34fbc6fbe246
Gregory Brancq <gregory.brancq@free.fr>
2019-12-01 17:00:00 +0100

    Added script to reset screen configuration in one click

--- scripts/ecran.sh
+++ scripts/ecran.sh
@@ -1,7 +1,15 @@
 #!/bin/zsh
 
-#xrandr --output DVI-I-1 --off
-xrandr --output HDMI-0 --off
+# #xrandr --output DVI-I-1 --off
+# xrandr --output HDMI-0 --off
+# sleep 5s
+# #xrandr --auto --output DVI-I-1 --mode 1680x1050 --right-of HDMI-0
+# xrandr --auto --output HDMI-0 --mode 1680x1050 --left-of DVI-I-1
+
+gnome-control-center display &
+proc_gcc=`ps -C gnome-control-center -o pid=`
+
 sleep 5s
-#xrandr --auto --output DVI-I-1 --mode 1680x1050 --right-of HDMI-0
-xrandr --auto --output HDMI-0 --mode 1680x1050 --left-of DVI-I-1
+
+kill -9 $proc_gcc
+
