b3d7a58dd9ed1be2e0e4831810bb2c7c9a800460
Gregory Brancq <gregory.brancq@free.fr>
2020-01-14 17:04:43 +0100

    Corrected script for private @

--- getIp.sh
+++ getIp.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -f
 
-echo ; date | cut -f 2 ; echo '##### @IP Publique #####' ; wget http://checkip.dyndns.org/ -O - -o /dev/null | cut -d: -f 2 | cut -d\< -f 1 ; echo '##### @IP Privée eth #####' ; ifconfig | grep Masque | cut -d i -f 2
+echo '##### @IP Publique #####'
+wget http://checkip.dyndns.org/ -O - -o /dev/null | cut -d: -f 2 | cut -d\< -f 1 ; 
 
-sleep 100
+echo '##### @IP Privée eth #####'
+ifconfig | grep netmask | cut -d i -f 2
+
