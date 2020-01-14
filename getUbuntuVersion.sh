c1f84dfb459557062fe34936be9349c2c9c529b8
Gregory Brancq <gregory.brancq@free.fr>
2020-01-14 17:23:30 +0100

    Added useful information for getUbuntuVersion

--- getUbuntuVersion.sh
+++ getUbuntuVersion.sh
@@ -2,5 +2,9 @@
 
 lsb_release -a
 
+echo "Kernel name:   " `uname -s`
+echo "Kernel release:" `uname -r`
+echo "Processor:     " `uname -p`
 echo "32 or 64 bits: " `getconf LONG_BIT`
+echo "OS:            " `uname -o`
 
