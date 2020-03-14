46e369f7b323e9f198c9eb75825ebe8ccb8f1604
Gregory Brancq <gregory.brancq@free.fr>
2020-03-14 12:16:20 +0100

    Added command for screens in the bedroom

--- ecran.sh
+++ ecran.sh
@@ -7,8 +7,8 @@
 # xrandr --auto --output HDMI-0 --mode 1680x1050 --left-of DVI-I-1
 #
 #cvt 1680 1050 60     pour connaitre les paramètres de la ligne suivante
-#xrandr --newmode "1680x1050_60.00"  146.25  1680 1784 1960 2240  1050 1053 1059 1089 -hsync +vsync
-#xrandr --addmode DP-2-1 1680x1050_60.00
+xrandr --newmode "1680x1050_60.00"  146.25  1680 1784 1960 2240  1050 1053 1059 1089 -hsync +vsync
+xrandr --addmode DP-2-1 1680x1050_60.00
 
 
 
